3b9473cdf4023966072af923356bde8b9db46036
mnicolesa <49413037+mnicolesa@users.noreply.github.com>
2019-09-26 15:45:03 -0230

    Add files via upload

---
+++ Lab2/PerfectPalindrome.php
@@ -0,0 +1,20 @@
+  
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Perfect Palindrome</title>
+</head>
+<body>
+	<h1>Perfect Palindrome Racecar</h1>
+	<?php
+		$palOne = strrev("racecar");
+		$palTwo = "racecar";
+		
+		if ($palTwo == $palOne) 
+		
+		{
+			echo "It is a palindrome!";
+		}
+	?>
+</body>
+</html>
